fef681c3adb9c0a9fe7955649b60eb61f2efc6f2
jobswouldneverdothis <157913715+jobswouldneverdothis@users.noreply.github.com>
2024-12-24 20:02:39 +0530

    minor change in the copy button's css class
    just changes the colour of the button when cursor is hovered

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ function App() {
         />
         <button
         onClick={copyPasswordToClip}
-        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 hover:bg-red-600'
         >copy</button>
         
     </div>
